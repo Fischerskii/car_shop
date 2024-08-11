3d00121ae64276212b458124bfdf608b54d08bd4
Pavel <p.al.trofimov@gmail.com>
2024-08-11 20:16:43 +0300

    some changes in changelog and move db connect to properties

--- src/main/java/ru/ylab/hw1/config/DatabaseConfig.java
+++ src/main/java/ru/ylab/hw1/config/DatabaseConfig.java
@@ -1,16 +1,34 @@
 package ru.ylab.hw1.config;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
+@Slf4j
 public class DatabaseConfig {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/car_shop_db";
-    private static final String USER = "car_shop_user";
-    private static final String PASSWORD = "password";
+    private static final Properties properties = new Properties();
+
+    static {
+        String propertiesFilePath = "src/main/resources/application.properties";
+        try (FileInputStream input = new FileInputStream(propertiesFilePath)) {
+            properties.load(input);
+        } catch (IOException e) {
+            log.error("Failed to load properties file from path: {}", propertiesFilePath, e);
+            throw new RuntimeException("Failed to load properties file", e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        String url = properties.getProperty("db.url");
+        String user = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
